c24262ec1c833c597925343a7ad7c4959f4835db
Cy-2004 <cy2004@Chelsea>
2024-11-19 12:50:21 -0500

    finished part 4

--- app/main.py
+++ app/main.py
@@ -16,7 +16,15 @@ DB = "gzd2yk"
 
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur=db.cursor()
+
 api = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @api.get("/")  # zone apex
 def zone_apex():
@@ -42,4 +50,32 @@ async def get_payload(request: Request):
     sum = num1 + num2
     return {"sum":sum}
     # return await request.json()
+
+@api.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@api.get('/songs')
+def get_songs():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres ON songs.genre=genres.genreID;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
