9cdfab3258b3a3a51750578767eb7373ca0a8a67
Cy-2004 <cy2004@Chelsea>
2024-11-19 12:20:14 -0500

    step 4

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import pandas as pd
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "gzd2yk"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 api = FastAPI()
 
 @api.get("/")  # zone apex
@@ -15,3 +25,21 @@ def zone_apex():
 @api.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@api.get("customer/{idx}")
+def get_customer(idx: int):
+    # read the data from csv file
+    df = pd.read_csv("../customers.csv")
+    # filter data based on the index
+    customer = df.iloc[idx]
+    return customer.to_dict()
+
+@api.post("/get_paylod") # push stuff to api
+async def get_payload(request: Request):
+    response = await request.json()
+    num1 = response.get("num1")
+    num2 = response.get("num2")
+    sum = num1 + num2
+    return {"sum":sum}
+    # return await request.json()
+
